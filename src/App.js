8c36538e434a1abed94673606cb7bdfb7e5f5c00
FLP13 <fwallden@gmail.com>
2021-04-11 13:46:43 +0200

    changed rows and columns

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { PuzzleArea } from './components/PuzzleArea';
 export const App = () => {
   return (
     <Content>
-      <PuzzleArea rows="2" columns="2" />
+      <PuzzleArea rows="3" columns="3" />
     </Content>
   );
 }
